bc19581eaec4383aac1ff7954eff628e8e952e67
Mehran Norouzi <mehrannoruzi@gmail.com>
2020-08-28 19:45:27 +0430

    Add New TeleChatID

--- RasmiOnline.Business/GlobalVariable.cs
+++ RasmiOnline.Business/GlobalVariable.cs
@@ -7,6 +7,6 @@
         public static string LogPath { get { return $"{AppDomain.CurrentDomain.BaseDirectory}\\Log"; } }
         public static string ObserverConfig { get { return $"{AppDomain.CurrentDomain.BaseDirectory}\\ObserverConfig.json"; } }
 
-        public static string AdminInstanceId = "-1001128896026";
+        public static string AdminInstanceId = "-1001475828693";
     }
 }
